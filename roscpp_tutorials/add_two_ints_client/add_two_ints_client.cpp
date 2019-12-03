0f17a32d26bfd4b405b49fc2f5e911a1082024f1
MarqRazz <marq.razz@gmail.com>
2019-12-03 14:06:19 -0700

    Update add_two_ints_client.cpp

--- roscpp_tutorials/add_two_ints_client/add_two_ints_client.cpp
+++ roscpp_tutorials/add_two_ints_client/add_two_ints_client.cpp
@@ -33,7 +33,7 @@
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "add_two_ints_client");
-  if (argc != 4)
+  if (argc != 3)
   {
     ROS_INFO("usage: add_two_ints_client X Y");
     return 1;
